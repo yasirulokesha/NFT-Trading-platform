52b562f01ce659bd1b575ab0eae8025cdb68e48d
Yasiru Lokesha <47551431+yasirulokesha@users.noreply.github.com>
2025-05-26 01:23:46 +1000

    Update database.py

--- src/API/database.py
+++ src/API/database.py
@@ -1,6 +1,6 @@
 # Config the database credintials
 db_config = {
-    "host": "1.145.191.169",
+    "host": "tcp:opentrade.database.windows.net,1433",
     "user": "opentrade_server",
     "password": "trading@2024",
     "database": "trading@2024"
